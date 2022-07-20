4cf544ad4a3afb49c0a2e39a46a818cb56b19642
admin <admin@formtec-international.com>
2022-07-20 14:35:04 -0400

    Custom authentication

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::auth();
 Route::post('/login', 'Auth\AuthController@passwordLogin');
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
