db2ce5507a91e44f7741d06d08d4c9278885921b
admin <admin@formtec-international.com>
2022-07-26 21:54:02 -0400

    Updating backend relationships

---
+++ database/migrations/2022_07_16_000000_create_event_photo_mapping_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_photo', function (Blueprint $table) {
+            $table->increments('id');
+            // Identification Fields
+            $table->foreignId('event_id')->unsigned(); //Has Foreign
+            $table->foreignId('photo_id')->unsigned();
+
+            // Foreign Keys
+            $table->foreign('event_id')
+                ->references('id')
+                ->on('events')
+                ->onDelete('cascade');
+
+            // Foreign Keys
+            $table->foreign('photo_id')
+                ->references('id')
+                ->on('photos')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_photo');
+    }
+};
