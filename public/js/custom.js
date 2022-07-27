a90f2897bf825691673fe990395664803a6ee66c
admin <admin@formtec-international.com>
2022-07-27 00:08:39 -0400

    Updating backend relationships

--- public/js/custom.js
+++ public/js/custom.js
@@ -16,6 +16,9 @@ function showSlides() {
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
+
+    console.log(slides[slideIndex-1]);
+
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
