6bea09b4ce37aaed728e8f3ed88d23043e1bee85
admin <admin@formtec-international.com>
2022-07-26 23:05:18 -0400

    Updating backend relationships

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,8 +95,8 @@
                                          style="background: url({{ $day[3] }})"
                                          @else
                                          style="background: linear-gradient(
-                                             rgba(0, 0, 0, 0.3),
-                                             rgba(0, 0, 0, 0.3)
+                                             rgba(0, 0, 0, 0.2),
+                                             rgba(0, 0, 0, 0.2)
                                              ), url({{ $day[3] }})"
                                          @endif
                                          class="date active" onclick="setDate(this.id); getEvents(this.id)">
