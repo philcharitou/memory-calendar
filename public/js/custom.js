d7886a8a57678ef8bd7bc61032fd3c4c045f2fe2
admin <admin@formtec-international.com>
2022-07-27 00:09:04 -0400

    Updating backend relationships

--- public/js/custom.js
+++ public/js/custom.js
@@ -17,7 +17,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
 
-    console.log(slides[slideIndex-1]);
+    console.log(slides);
 
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
